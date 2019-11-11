ce56f4ec6190dd565471676863afd19fcdaf0c1a
tesschung <geobera0910@naver.com>
2019-11-11 13:41:06 +0900

    vue_cli

---
+++ Vue_js/03_vue_cli/youtube-browser/src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+    <!-- step 3: 사용 self-closing 반드시 필요-->
+    <SearchBar @inputChange="onInputChange"/>
+  </div>
+</template>
+
+<script>
+// step 1: import 호출
+import SearchBar from './components/SearchBar'
+
+// Vue
+export default {
+  name: 'App',  // Component의 이름을 저장
+  // step 2: components에 아래와 같이 등록
+  components: {
+    SearchBar,
+  },
+  methods: {
+    // inputChange라는 이벤트가 발생할때마다 실행
+    onInputChange: function(inputValue) {
+      console.log(inputValue)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue_js/03_vue_cli/youtube-browser/src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div>
+    <!-- input의 값이 바뀔때마다 onInput method를 실행한다. -->
+    <input type="text" v-on:input="onInput"/>
+  </div>
+</template>
+
+
+<script>
+// methods 를 등록
+export default {
+  name: 'SearchBar',
+  methods: {
+    // // inputChange 이벤트가 발생했음을 알린다. 그리고 input의 value를 넘겨준다.
+    onInput: function (event) { 
+      this.$emit('inputChange', event.target.value) 
+      // console.log(event.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
